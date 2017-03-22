7c41fa2454f6daa9bebfe8d1a16dc03e376f35fa
Watcha Pon <watchapon@watchas-mac.local>
2017-03-22 02:17:14 -0700

    Initial Commit

---
+++ iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Watcha Pon on 3/22/17.
+//  Copyright © 2017 Indie Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
